5af8c8648488c01c85976f3e08dcca695f0b9244
rolfslau <rolfslau@byu.edu>
2025-02-26 13:34:16 -0700

    added navigation to books on login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export function Login() {
+  const navigate = useNavigate()
   const [username, setusername] = React.useState("")
   const [password, setpassword] = React.useState("")
   function changeusernametext(e) {
     setusername(e.target.value)
   }
   function registeruser() {
-    console.log("logged in")
+    localStorage.setItem('username', username)
+    localStorage.setItem('password', password)
+  }
+  function loginuser() {
+    if (username && password) {
+      // localStorage.getItem('username')
+      const expectedpassword = localStorage.getItem('password')
+      if (expectedpassword == password) {
+        navigate("/books")
+      }
+      else {
+        alert("bad login")
+      }
+    }
   }
   return (
     <main>
       <div>{username}</div>
       <div>{password}</div>
-        <div class="username">
+        <div className="username">
         username:
         <input type="username" onChange={changeusernametext}></input>
         </div>
         <br></br>
         <br/>
-        <div class="password">
+        <div className="password">
         password:
         <input type="password" onChange={(e)=>setpassword(e.target.value)}/>
         </div>
 
         
         <button onClick={registeruser}>register</button>
+        <button onClick={loginuser}>login</button>
         
     </main>
   );
