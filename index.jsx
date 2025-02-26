5b73d7bad2e230025f1bc783f29487b60d9e60ec
rolfslau <rolfslau@byu.edu>
2025-02-26 13:17:08 -0700

    added reactivity to login/password

--- index.jsx
+++ index.jsx
@@ -4,3 +4,21 @@ import App from './src/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+function Login() {
+    const [text, setText] = React.useState('');
+    function loginuser() {
+        localStorage.setItem('user', text)
+    }
+
+    function textChange(e) {
+        setText(e.target.value)
+    }
+    return (
+        <div className='page'>
+            <h1>Login</h1>
+            <input type='text' onChange={textChange} />
+            <button onClick={loginuser}>Login</button>
+        </div>
+    )
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function Login() {
+  const [username, setusername] = React.useState("")
+  const [password, setpassword] = React.useState("")
+  function changeusernametext(e) {
+    setusername(e.target.value)
+  }
+  function registeruser() {
+    console.log("logged in")
+  }
   return (
     <main>
+      <div>{username}</div>
+      <div>{password}</div>
         <div class="username">
         username:
-        <input type="username"></input>
+        <input type="username" onChange={changeusernametext}></input>
         </div>
         <br></br>
         <br/>
         <div class="password">
         password:
-        <input type="password"/>
+        <input type="password" onChange={(e)=>setpassword(e.target.value)}/>
         </div>
 
-        <NavLink className='button' to="books">login</NavLink>
+        
+        <button onClick={registeruser}>register</button>
         
     </main>
   );
